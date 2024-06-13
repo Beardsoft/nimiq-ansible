9e73920b2f3e9edf289f4f19fca1f8f4934a21fe
maestroi <meastro@protonmail.com>
2024-06-14 01:53:51 +0200

    feat(): update setup script

--- setup.sh
+++ setup.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
 # This script sets up or updates the Nimiq node using Ansible
-# Usage: curl -s https://raw.githubusercontent.com/maestroi/nimiq-ansible/master/setup.sh | bash -s <network> <node_type>
+# Usage: curl -s https://raw.githubusercontent.com/maestroi/nimiq-ansible/master/setup.sh | bash -s [<network>] [<node_type>]
 
 set -e
 
-# Check for required arguments
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <network> <node_type>"
-    exit 1
-fi
-
-NETWORK=$1
-NODE_TYPE=$2
+# Set default values
+NETWORK=${1:-testnet}
+NODE_TYPE=${2:-validator}
 
 # Function to install necessary dependencies
 install_dependencies() {
+    echo "Updating OS packages..."
+    apt-get update -y > /dev/null
     echo "Installing necessary dependencies..."
-    apt-get update -y
-    apt-get install -y software-properties-common
+    apt-get install -y software-properties-common > /dev/null
 }
 
 # Function to install Ansible
 install_ansible() {
     echo "Adding Ansible repository..."
-    apt-add-repository --yes --update ppa:ansible/ansible
+    apt-add-repository --yes --update ppa:ansible/ansible > /dev/null
     echo "Installing Ansible..."
-    apt-get install -y ansible
+    apt-get install -y ansible > /dev/null
 }
 
 # Function to clone or update the Nimiq Ansible repository
@@ -34,10 +30,10 @@ update_repository() {
     if [ -d "/opt/nimiq-ansible" ]; then
         echo "Updating the Nimiq Ansible repository..."
         cd /opt/nimiq-ansible
-        git pull
+        git pull > /dev/null
     else
         echo "Cloning the Nimiq Ansible repository..."
-        git clone https://github.com/maestroi/nimiq-ansible.git /opt/nimiq-ansible
+        git clone https://github.com/maestroi/nimiq-ansible.git /opt/nimiq-ansible > /dev/null
         cd /opt/nimiq-ansible
     fi
 }
@@ -45,7 +41,15 @@ update_repository() {
 # Function to run the Ansible playbook
 run_playbook() {
     echo "Running the Ansible playbook..."
-    ansible-playbook -i localhost, -c local ansible/playbook.yml --extra-vars "network=${NETWORK} node_type=${NODE_TYPE}"
+    ansible-playbook -i localhost, -c local /opt/nimiq-ansible/playbook.yml --extra-vars "network=${NETWORK} node_type=${NODE_TYPE}"
+}
+
+# Function to create symbolic link
+create_symlink() {
+    if [ ! -L /usr/local/bin/nimiq-setup ]; then
+        echo "Creating symbolic link for setup script..."
+        ln -s /opt/nimiq-ansible/setup.sh /usr/local/bin/nimiq-setup
+    fi
 }
 
 # Install necessary dependencies
@@ -61,5 +65,8 @@ update_repository
 
 # Run the Ansible playbook
 run_playbook
+
+# Create symbolic link
+create_symlink
 
 echo "Nimiq node setup/update complete!"
